9488b7b3faee04befb5a6552f739f1757f06ae70
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-20 10:28:31 -0400

    basic cmds for openpyx1

--- iss.py
+++ iss.py
@@ -1,5 +1,7 @@
 import requests
 # Importing library to make HTTP Requests
+import openpyxl
+# Importing library to create a new excel workbook
 
 response = requests.get('http://api.open-notify.org/iss-now.json') # HTTP GET method, fetches data and saves it in response var
 iss_data = response.json() # Parsing json response and storing it in iss_data 
@@ -13,3 +15,28 @@ long = iss_position['longitude']
 
 print(lat)
 print(long)
+
+# Creating a new Excel workbook
+iss_workbook = openpyxl.Workbook()
+
+# Creating a new worksheet
+worksheet = iss_workbook.active
+
+# Change the title of the worksheet
+worksheet.title = "{}Coordinates"
+
+# Add data to the worksheet
+worksheet['A1'] = "lat"
+worksheet['B1'] = "long"
+
+worksheet['A2'] = lat
+worksheet['B2'] = long
+
+# Save the workbood to a file
+iss_workbook.save('{}_iss_coordinates')
+
+# Close the workbook
+iss_workbook.close()
+
+# 9/20/23
+# installed openpyx1 library
