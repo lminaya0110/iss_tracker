47d58d0d27a93a517d830968504af4e81635787c
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-20 12:31:42 -0400

    working excel generator

--- iss.py
+++ iss.py
@@ -4,47 +4,50 @@ from datetime import datetime
 # Importing requests library to make HTTP Requests, openpyx1 library to create a new excel workbook,
 # datetime module from standard python library for dates and times
 
-
+# API CALL 
 response = requests.get('http://api.open-notify.org/iss-now.json') # HTTP GET method, fetches data and saves it in response var
 iss_data = response.json() # Parsing json response and storing it in iss_data 
 iss_position = iss_data['iss_position'] # Extracts position object from json data
 
 # Extract latitude and longitude values from the iss pos object, and stores them in
 # the lat and long variables respectively
-
 lat = iss_position['latitude'] 
 long = iss_position['longitude']
 
 print(lat)
 print(long)
+# -------------------------------------------------------------------------------------------------------------------------
 
-# vv method used to format d & t as a string according to specified format (YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
-datestring = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M_%S')
+# EXCEL
+
+# vv method used to format d & t as a string according to specified format (YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_Locale's AM or PM)
+datestring = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M_%S_%p')
 print(datestring)
 
-def create_excel():
-    # Creating a new Excel workbook
-    iss_workbook = openpyxl.Workbook()
 
-    # Creating a new worksheet
-    worksheet = iss_workbook.active
 
-    # Change the title of the worksheet
-    worksheet.title = "{datestring}_coordinates"
 
-    # Add data to the worksheet
-    worksheet['A1'] = "lat"
-    worksheet['B1'] = "lng"
+# Creating a new Excel workbook
+iss_workbook = openpyxl.Workbook()
 
-    worksheet['A2'] = lat
-    worksheet['B2'] = long
+# Creating a new worksheet
+worksheet = iss_workbook.active
 
-    # Save the workbood to a file
-    iss_workbook.save('{}_iss_coordinates')
+# Change the title of the worksheet
+worksheet.title = datestring
 
-    # Close the workbook
-    iss_workbook.close()
-    
+# Add data to the worksheet
+worksheet['A1'] = "lat"
+worksheet['B1'] = "lng"
 
-# 9/20/23
-# installed openpyx1 library
+worksheet['A2'] = lat
+worksheet['B2'] = long
+
+# Save the workbook to a file
+iss_workbook.save(f'{datestring}.xlsx')
+
+# Close the workbook
+iss_workbook.close()
+
+# -------------------------------------------------------------------------------------------------------------------------
+
