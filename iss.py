51215fdca7bf098ed20d1cfe0ba272098e1fcf17
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-20 11:17:58 -0400

    adjusted datetime format, added func

--- iss.py
+++ iss.py
@@ -1,7 +1,9 @@
 import requests
-# Importing library to make HTTP Requests
 import openpyxl
-# Importing library to create a new excel workbook
+from datetime import datetime
+# Importing requests library to make HTTP Requests, openpyx1 library to create a new excel workbook,
+# datetime module from standard python library for dates and times
+
 
 response = requests.get('http://api.open-notify.org/iss-now.json') # HTTP GET method, fetches data and saves it in response var
 iss_data = response.json() # Parsing json response and storing it in iss_data 
@@ -16,27 +18,33 @@ long = iss_position['longitude']
 print(lat)
 print(long)
 
-# Creating a new Excel workbook
-iss_workbook = openpyxl.Workbook()
+# vv method used to format d & t as a string according to specified format (YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+datestring = datetime.strftime(datetime.now(), '%Y_%m_%d_%H_%M_%S')
+print(datestring)
 
-# Creating a new worksheet
-worksheet = iss_workbook.active
+def create_excel():
+    # Creating a new Excel workbook
+    iss_workbook = openpyxl.Workbook()
 
-# Change the title of the worksheet
-worksheet.title = "{}Coordinates"
+    # Creating a new worksheet
+    worksheet = iss_workbook.active
 
-# Add data to the worksheet
-worksheet['A1'] = "lat"
-worksheet['B1'] = "lng"
+    # Change the title of the worksheet
+    worksheet.title = "{datestring}_coordinates"
 
-worksheet['A2'] = lat
-worksheet['B2'] = long
+    # Add data to the worksheet
+    worksheet['A1'] = "lat"
+    worksheet['B1'] = "lng"
 
-# Save the workbood to a file
-iss_workbook.save('{}_iss_coordinates')
+    worksheet['A2'] = lat
+    worksheet['B2'] = long
 
-# Close the workbook
-iss_workbook.close()
+    # Save the workbood to a file
+    iss_workbook.save('{}_iss_coordinates')
+
+    # Close the workbook
+    iss_workbook.close()
+    
 
 # 9/20/23
 # installed openpyx1 library
