c01c9f419daf323f6f87d37789047ddf718fdc90
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-20 10:15:53 -0400

    added comments

--- iss.py
+++ iss.py
@@ -1,10 +1,14 @@
 import requests
+# Importing library to make HTTP Requests
 
-response = requests.get('http://api.open-notify.org/iss-now.json')
-iss_data = response.json()
-iss_position = iss_data['iss_position']
+response = requests.get('http://api.open-notify.org/iss-now.json') # HTTP GET method, fetches data and saves it in response var
+iss_data = response.json() # Parsing json response and storing it in iss_data 
+iss_position = iss_data['iss_position'] # Extracts position object from json data
 
-lat = iss_position['latitude']
+# Extract latitude and longitude values from the iss pos object, and stores them in
+# the lat and long variables respectively
+
+lat = iss_position['latitude'] 
 long = iss_position['longitude']
 
 print(lat)
